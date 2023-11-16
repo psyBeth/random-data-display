af801d703b3cdc1a116ae055de0399494e293243
Betul <betulkoru06@gmail.com>
2023-11-17 01:09:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,168 @@
+const userData = {
+    "results": [
+        {
+            "gender": "female",
+            "name": {"title": "Ms", "first": "Malathy", "last": "Mardhekar"},
+            "location": {
+                "street": {"number": 9422, "name": "Maharanipeta"},
+                "city": "Pondicherry",
+                "state": "Puducherry",
+                "country": "India",
+                "postcode": 99932,
+                "coordinates": {"latitude": "8.3415", "longitude": "71.6176"},
+                "timezone": {"offset": "-9:00", "description": "Alaska"}
+            },
+            "email": "malathy.mardhekar@example.com",
+            "login": {
+                "uuid": "57d2f7bc-f608-424f-8ada-f565f10b072d",
+                "username": "purplezebra382",
+                "password": "sunnyday",
+                "salt": "AaGjt9cP",
+                "md5": "c14d9ca6835bbbd95da1341f88820fd7",
+                "sha1": "1e88a086d17565848a60625ef6335f1d86cf3ee9",
+                "sha256": "63821259ac277cf81376fde6bbaafb32a53c4d898e3e5b3b66e9eef9fc396dce"
+            },
+            "dob": {"date": "1967-12-11T03:23:23.406Z", "age": 55},
+            "registered": {"date": "2009-05-27T13:30:21.542Z", "age": 14},
+            "phone": "7070418709",
+            "cell": "9465094626",
+            "id": {"name": "UIDAI", "value": "650971389730"},
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/93.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/93.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/93.jpg"
+            },
+            "nat": "IN"
+        },
+        {
+            "gender": "male",
+            "name": {"title": "Mr", "first": "Alberto", "last": "Oliveira"},
+            "location": {
+                "street": {"number": 7206, "name": "Travessa dos Martírios"},
+                "city": "Paulista",
+                "state": "Bahia",
+                "country": "Brazil",
+                "postcode": 28635,
+                "coordinates": {"latitude": "-75.9830", "longitude": "-45.9417"},
+                "timezone": {"offset": "+11:00", "description": "Magadan, Solomon Islands, New Caledonia"}
+            },
+            "email": "alberto.oliveira@example.com",
+            "login": {
+                "uuid": "0bfbfa23-93f0-4ace-9a7a-0f0bc55fd575",
+                "username": "heavyostrich137",
+                "password": "nation",
+                "salt": "Lm0dQX32",
+                "md5": "1a3f53cf5a648ef1a6baca93f5940acb",
+                "sha1": "5428e3217dbf63de9988da86a7f89cd3eedb179d",
+                "sha256": "a77a75d85dbb385a3a4168546667db91a6e7f866b9ebd7030d93c7bf2ff5c01f"
+            },
+            "dob": {"date": "1999-01-18T23:04:49.621Z", "age": 24},
+            "registered": {"date": "2002-04-24T01:42:02.956Z", "age": 21},
+            "phone": "(83) 3983-4325",
+            "cell": "(89) 7615-1125",
+            "id": {"name": "CPF", "value": "570.659.577-96"},
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/69.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/69.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/69.jpg"
+            },
+            "nat": "BR"
+        },
+        {
+            "gender": "female",
+            "name": {"title": "Ms", "first": "Jasmine", "last": "Anderson"},
+            "location": {
+                "street": {"number": 7401, "name": "Moorhouse Avenue"},
+                "city": "Tauranga",
+                "state": "Manawatu-Wanganui",
+                "country": "New Zealand",
+                "postcode": 33683,
+                "coordinates": {"latitude": "48.9688", "longitude": "126.2884"},
+                "timezone": {"offset": "+8:00", "description": "Beijing, Perth, Singapore, Hong Kong"}
+            },
+            "email": "jasmine.anderson@example.com",
+            "login": {
+                "uuid": "70fdb8c5-4b42-41eb-8fb4-c9ea2fc9ee7b",
+                "username": "crazypeacock619",
+                "password": "pippin",
+                "salt": "Ssid5Tp9",
+                "md5": "b7c15af65038c38fba21f6c6d18647ee",
+                "sha1": "1ff4858120a9cf51c602972f66c5a8d086f93aa5",
+                "sha256": "108931d83a1537ed75b6bf1e042b542ac7208540226d928f7405470e038ee691"
+            },
+            "dob": {"date": "1994-05-02T02:39:28.196Z", "age": 29},
+            "registered": {"date": "2015-04-15T12:22:52.952Z", "age": 8},
+            "phone": "(986)-994-7978",
+            "cell": "(589)-042-2226",
+            "id": {"name": "", "value": null},
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/17.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/17.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/17.jpg"
+            },
+            "nat": "NZ"
+        },
+        {
+            "gender": "female",
+            "name": {"title": "Mademoiselle", "first": "Christa", "last": "Nicolas"},
+            "location": {
+                "street": {"number": 9620, "name": "Avenue du Fort-Caire"},
+                "city": "Emmetten",
+                "state": "Schwyz",
+                "country": "Switzerland",
+                "postcode": 3563,
+                "coordinates": {"latitude": "-62.3551", "longitude": "46.6015"},
+                "timezone": {"offset": "+8:00", "description": "Beijing, Perth, Singapore, Hong Kong"}
+            },
+            "email": "christa.nicolas@example.com",
+            "login": {
+                "uuid": "2c1df14a-d7ea-4846-a985-5448e141b1ce",
+                "username": "greenfrog343",
+                "password": "ladybug",
+                "salt": "E5PGrxkM",
+                "md5": "78776935faf121dcda599c81460eb46a",
+                "sha1": "c013c331548dc0b11c5af19bd0032409257b6e2f",
+                "sha256": "aef8e398336bd0d845840c0d4840c96a311932fa6a36162b54761b2ca65610b3"
+            },
+            "dob": {"date": "1960-06-11T03:47:19.221Z", "age": 63},
+            "registered": {"date": "2013-07-20T23:23:58.906Z", "age": 10},
+            "phone": "075 707 95 89",
+            "cell": "077 268 67 35",
+            "id": {"name": "AVS", "value": "756.3411.1367.32"},
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/84.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/84.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/84.jpg"
+            },
+            "nat": "CH"
+        }
+    ],
+    "info": {"seed": "6dad456e979dd8e5", "results": 1, "page": 1, "version": "1.4"}
+};
+
+let currentUserIndex = 0;
+
+        function updateContainer() {
+            const user = userData.results[currentUserIndex];
+            const container = document.getElementById('user-container');
+
+            const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+
+            container.innerHTML = `
+                <img src="${user.picture.thumbnail}" alt="User Thumbnail">
+                <h2>${fullName}</h2>
+                <p>Email: ${user.email}</p>
+                <p>Phone: ${user.phone}</p>
+            `;
+        }
+
+       
+        updateContainer();
+
+        
+        document.getElementById('user-container').addEventListener('click', function () {
+            
+            currentUserIndex = (currentUserIndex + 1) % userData.results.length;
+            
+            updateContainer();
+        });
